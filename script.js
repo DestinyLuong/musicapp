543ccd6a10e349a5a1b283f4be685cd8e415acd5
Destiny Luong <destinyul2010@gmail.com>
2018-03-21 23:40:07 +0000

    Start NavBar

---
+++ script.js
@@ -0,0 +1,21 @@
+/* global $ */
+
+function getUserInput(){
+
+}
+
+function getURL(){
+    
+}
+
+function getAPI(){
+    
+}
+
+function displayResults(){
+    
+}
+
+$(document).ready( function(){
+    
+}); 
