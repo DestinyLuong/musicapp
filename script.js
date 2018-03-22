6ecbe8fdf19a8637cdf89b9e639d74e1a91d6d02
Destiny Luong <destinyul2010@gmail.com>
2018-03-22 00:24:29 +0000

    FUnctions

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 /* global $ */
 
 function getUserInput(){
+/*This function GETS and STORES the VALUE of the USER INPUT*/
 
 }
 
 function getURL(){
-    
+/*This function uses "getUserInput()" and MERGES it WITH the URL*/
 }
 
 function getAPI(){
-    
+/* This function uses "getURL()" and GRAB the items and STORES the VALUE*/
 }
 
 function displayResults(){
-    
+/* This function uses "getAPI() and uses a FORLOOP to APPEND it to the page"  */
 }
 
 $(document).ready( function(){
